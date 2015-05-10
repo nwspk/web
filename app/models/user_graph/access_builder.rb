65dd6be87408096f1878d352a15e90d39c620d43
Eugen Rochko <eugen@zeonfederated.com>
2015-05-10 13:54:51 +0200

    Door access builder corrected

--- app/models/user_graph/access_builder.rb
+++ app/models/user_graph/access_builder.rb
@@ -2,36 +2,25 @@ class UserGraph::AccessBuilder < UserGraph::Builder
   def initialize(options = {})
     @start_date  = options[:start]     || 1.day.ago
     @end_date    = options[:end]       || 1.day.from_now
-    @max_depth   = options[:max_depth] || 5
   end
 
   def build
-    user_subset = DoorAccess.where(created_at: @start_date..@end_date).select('distinct user_id').pluck(:user_id)
+    user_subset_ids = DoorAccess.where(created_at: @start_date..@end_date).select('distinct user_id').pluck(:user_id)
+    user_subset     = User.where(id: user_subset_ids)
 
-    return empty_graph if user_subset.size == 0
+    return empty_graph if user_subset.size < 1
 
-    offset               = Random.rand(user_subset.size)
-    random_starting_user = User.offset(offset).first
+    graph       = UserGraph::Graph.new(nil)
+    friendships = FriendEdge.where(from_id: user_subset_ids).weighted.includes(:from)
 
-    graph       = UserGraph::Graph.new(random_starting_user)
-    open_list   = []
-    closed_list = []
+    user_subset.each do |u|
+      graph.nodes << u
+    end
 
-    open_list   << [random_starting_user, 0]
-    graph.nodes << random_starting_user
-
-    while !open_list.empty?
-      cur, depth = open_list.pop
-
-      closed_list << cur.id
-
-      get_child_nodes(cur).each do |f|
-        graph.edges << [cur.id, f.to_id, f.weight]
-        graph.nodes << f.to
-
-        unless closed_list.include?(f.to_id) || (depth + 1) > @max_depth
-          open_list << [f.to, depth + 1]
-        end
+    friendships.each do |f|
+      if user_subset_ids.include? f.to_id
+        graph.edges << [f.from_id, f.to_id, f.weight]
+        graph.edges << [f.to_id, f.from_id, f.weight]
       end
     end
 
@@ -42,9 +31,5 @@ class UserGraph::AccessBuilder < UserGraph::Builder
 
   def empty_graph
     UserGraph::Graph.new(nil)
-  end
-
-  def get_child_nodes(cur)
-    cur.friends.weighted
   end
 end
