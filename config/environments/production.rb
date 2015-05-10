046f4209fd29089b7ce9fb68c90aeb9bf5e0ad73
Eugen Rochko <eugen@zeonfederated.com>
2015-05-10 18:08:44 +0200

    Fixed mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,12 +79,15 @@ Rails.application.configure do
 
   # Mail delivery
   config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'nwspk.herokuapp.com',
+    :domain         => 'www.nwspk.com',
     :authentication => :plain,
   }
+
+  config.action_mailer.default_url_options = { host: 'www.nwspk.com' }
 end
