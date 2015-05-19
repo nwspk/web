83df46c389c7b9c5bd814924aa4f77093ae3ff4f
Eugen Rochko <eugen@zeonfederated.com>
2015-05-19 19:39:17 +0200

    Fruchterman Rheingold replaced by Force Atlas 2

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,6 +1,6 @@
 //= require sigma.min
 //= require plugins/sigma.parsers.json.min
 //= require plugins/sigma.plugins.animate.min
-//= require plugins/sigma.fruchterman
+//= require plugins/sigma.layout.forceAtlas2.min
 //= require sigma.customLabels
 
