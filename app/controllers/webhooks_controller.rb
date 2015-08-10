3d489c3f303d618f9f3d56ad32b22d76d67686e3
Eugen Rochko <eugen@zeonfederated.com>
2015-08-10 17:16:18 +0200

    Finally enabling billing e-mails

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -39,7 +39,7 @@ class WebhooksController < ApplicationController
   def on_invoice_paid(subscription, invoice)
     subscription.update!(active_until: 30.days.from_now)
     Payment.create!(user: subscription.user, stripe_invoice_id: invoice.id, total: invoice.total, date: invoice.date)
-    #UserMailer.billing_email(subscription.user).deliver_later
+    UserMailer.billing_email(subscription.user).deliver_later
   end
 
   def on_invoice_failed(subscription, invoice)
