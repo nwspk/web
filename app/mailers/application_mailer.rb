7a242264044c636c19b735abf6e820ae73e61fce
Eugen Rochko <eugen@zeonfederated.com>
2015-08-10 14:11:32 +0200

    Set return_path on outgoing e-mails to ed@nwspk.com

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "NWSPK <notifications@nwspk.com>"
+  default from: "NWSPK <notifications@nwspk.com>", return_path: "ed@nwspk.com"
   layout 'mailer'
 end
