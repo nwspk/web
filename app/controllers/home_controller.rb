7a2320c3be5f3598f6f0edf8412b3af08da8fd57
Eugen Rochko <eugen@zeonfederated.com>
2016-05-23 12:42:50 +0200

    Only count active subscriptions in pledge calc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @members = User.with_subscription.limit(3)
     @fellows = User.fellows.limit(8)
 
-    @total_pledged = Plan.all.reduce(Money.new(0, 'GBP')) { |aggr, plan| aggr + plan.value * plan.subscriptions.count * plan.contribution }
+    @total_pledged = Plan.all.reduce(Money.new(0, 'GBP')) { |aggr, plan| aggr + plan.value * plan.subscriptions.active.count * plan.contribution }
     @total_members = User.with_subscription.count
   end
 
