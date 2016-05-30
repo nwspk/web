2bd5c1ae3a4626f053069349fd5e25bd4fbdecc8
Eugen Rochko <eugen@zeonfederated.com>
2016-05-30 20:18:37 +0200

    Staff and admins can see private events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,17 @@
 class EventsController < ApplicationController
+  before_action :set_events, only: :index
+
   def index
-    @events = Event.public_and_confirmed.group_by { |e| e.start_at.strftime('%Y-%m-%d') }
+    @events = @events.group_by { |e| e.start_at.strftime('%Y-%m-%d') }
+  end
+
+  private
+
+  def set_events
+    if user_signed_in? && current_user.admin_or_staff?
+      @events = Event.confirmed
+    else
+      @events = Event.public_and_confirmed
+    end
   end
 end
