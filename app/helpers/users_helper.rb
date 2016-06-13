10e73f6c7e771034ff17aa74e12055451208fa79
Eugen Rochko <eugen@zeonfederated.com>
2016-06-13 21:01:38 +0200

    Fixed up membership (signup) page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def signup_fee
+    Money.new(SIGNUP_FEE, 'GBP').format
+  end
+
+  def base_plan_rate
+    Plan.first.nil? ? Money.new(0, 'GBP').format : Plan.first.value.format
+  end
 end
