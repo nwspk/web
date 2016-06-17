76c8ba07c5cafef64abb3d729740bde240a37757
Kim Lawrie <aeropoetics@gmail.com>
2016-06-17 19:26:14 +0100

    Restructured events page into column layout and remove multiple events per day functionality to allow for better mobile-first experience

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,16 +3,16 @@ class EventsController < ApplicationController
   before_action :set_events, only: :index
 
   def index
-    @events = @events.group_by { |e| e.start_at.strftime('%Y-%m-%d') }
+    @event = Event.public_and_confirmed.upcoming
   end
 
   private
 
   def set_events
     if user_signed_in? && current_user.admin_or_staff?
-      @events = Event.confirmed.upcoming
+      @event = Event.confirmed.upcoming
     else
-      @events = Event.public_and_confirmed.upcoming
+      @event = Event.public_and_confirmed.upcoming
     end
   end
 end
