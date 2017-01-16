1cf7acbe3e34a777a0135055955031bea5fd33dc
Eugen Rochko <eugen@zeonfederated.com>
2017-01-16 19:55:47 +0100

    Add request_id to logs, fix ChangePlanService throwing exception when subscription cannot be found (expired on Stripe side without notifying our db?)

--- app/services/change_plan_service.rb
+++ app/services/change_plan_service.rb
@@ -7,16 +7,33 @@ class ChangePlanService
     customer = Stripe::Customer.retrieve(subscription_record.customer_id)
 
     if subscription_record.subscription_id.blank?
-      subscription = customer.subscriptions.create(plan: subscription_record.plan.stripe_id)
-      subscription_record.update!(subscription_id: subscription.id, active_until: arbitrary_future_date)
+      create_new_subscription(subscription_record)
     else
-      subscription = customer.subscriptions.retrieve(subscription_record.subscription_id)
-      subscription.plan = subscription_record.plan.stripe_id
-      subscription.save
+      begin
+        subscription = customer.subscriptions.retrieve(subscription_record.subscription_id)
+      rescue Stripe::InvalidRequestError
+        subscription = nil
+      end
+
+      if subscription.nil?
+        create_new_subscription(subscription_record)
+      else
+        change_plan_on_existing_subscription(subscription, subscription_record)
+      end
     end
   end
 
   private
+
+  def create_new_subscription(subscription_record)
+    subscription = customer.subscriptions.create(plan: subscription_record.plan.stripe_id)
+    subscription_record.update!(subscription_id: subscription.id, active_until: arbitrary_future_date)
+  end
+
+  def change_plan_on_existing_subscription(subscription, subscription_record)
+    subscription.plan = subscription_record.plan.stripe_id
+    subscription.save
+  end
 
   def arbitrary_future_date
     30.days.from_now
