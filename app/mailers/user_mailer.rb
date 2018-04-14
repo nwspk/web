e24e8436a7e8c282997dd0e387bd25dc430d4535
tobeee <tobeee@gmail.com>
2018-04-14 20:06:30 +0100

    Fixes billing email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,19 +11,20 @@ class UserMailer < ApplicationMailer
     @num_new_connections = @user.friends.where('friend_edges.created_at > ?', time - 30.days).count('distinct to_id')
     @upcoming_events     = Event.public_and_confirmed.upcoming
     @fellows             = User.fellows
-
+    @total_amount        = @subscription.plan.value.to_f / 100
+    @vat_amount          = ( (@total_amount * 0.2)  / 1.2).round(2)
+    @net_amount          = (@total_amount - @vat_amount) .round(2)
+    
     unless @subscription.customer_id.blank?
       stripe_customer = Stripe::Customer.retrieve(@subscription.customer_id)
       @card_num       = stripe_customer.sources.retrieve(stripe_customer.default_source).last4
     else
       @card_num = 4242
     end
-
     @ascii_table = Terminal::Table.new rows: [
       ['Membership Tier:', (@subscription.plan.try(:name) || 'None'), @subscription.plan.try(:money_value)],
       ['Total:', '', @subscription.plan.try(:money_value)]
     ]
-
     mail to: user.email, subject: "This month at Newspeak House"
   end
 
