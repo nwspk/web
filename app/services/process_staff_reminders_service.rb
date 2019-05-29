22338713fec917d42df76a92a755c23e1cf5010b
tobeee <tobeee@gmail.com>
2019-05-29 19:13:05 +0100

    Adds check to ensure reminders only sent when key is set

--- app/services/process_staff_reminders_service.rb
+++ app/services/process_staff_reminders_service.rb
@@ -1,10 +1,13 @@
 class ProcessStaffRemindersService
   def call
-    StaffReminder.active.find_each do |r|
-      next unless r.due?(Time.now)
+    #Ensure this key exists on the production server
+    if ENV.has_key?("SEND_REMINDERS")
+      StaffReminder.active.find_each do |r|
+        next unless r.due?(Time.now)
 
-      m = r.pop!
-      AdminMailer.staff_reminder_email(r, m).deliver_later unless m.nil?
+        m = r.pop!
+        AdminMailer.staff_reminder_email(r, m).deliver_later unless m.nil?
+      end
     end
   end
 end
