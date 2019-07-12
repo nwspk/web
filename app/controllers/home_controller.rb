5499c64dc741ddbb2009df9593cd84a637452f76
tobeee <tobeee@gmail.com>
2019-07-12 19:06:00 +0100

    Loads dean bio from DB

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,8 @@ class HomeController < ApplicationController
     @events  = Event.public_and_confirmed.upcoming
     @members = User.recent.limit(4).includes(:twitter, :facebook)
     @fellows = User.fellows.includes(:twitter, :facebook)
-
+    @dean = User.find(ENV['DEAN_USER_ID'].to_i)
+    
     @total_pledged = Plan.total_pledged
     @total_members = User.with_subscription.count
     @past_events = Event.public_and_confirmed.archive
