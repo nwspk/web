547f12f46f0570bbc47e7930392859c99e57a06e
Theodore Keloglou <zf@sirodoht.com>
2022-06-27 12:24:06 +0200

    remove connections routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   get 'account',                  to: 'dashboard#index', as: 'dashboard'
-  get '/auth/:provider/callback', to: 'connections#create'
-  get 'api/uid',                  to: 'api#uid'
-  get 'api/dividends',            to: 'api#dividends'
   get 'api/events',               to: 'api#events'
   get 'about',                    to: 'home#about', as: :about
   get 'fellowship',               to: 'home#fellowship', as: :fellowship
@@ -14,12 +11,6 @@ Rails.application.routes.draw do
   get 'residency',                to: 'home#residency', as: :residency
   get 'scholarships',             to: 'home#scholarships', as: :scholarships
   post 'webhooks',                to: 'webhooks#index'
-
-  resources :connections, only: [:destroy] do
-    collection do
-      get :check_friends
-    end
-  end
 
   resource :subscription, only: [:edit, :update, :destroy] do
     get :checkout
