1aff4e2cbba0e907d659544364b2a2d22d3d5372
Theodore Keloglou <zf@sirodoht.com>
2022-06-27 12:30:34 +0200

    fix account profile page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,3 +9,12 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require best_in_place
+//= require best_in_place.jquery-ui
+//= require bootstrap-sprockets
+$(document).ready(function() {
+  $(".best_in_place").best_in_place();
+});
