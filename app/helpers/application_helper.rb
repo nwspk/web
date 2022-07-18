85c87de32f8276327e92aa863e837ca1b84fdf7d
Theodore Keloglou <zf@sirodoht.com>
2022-07-18 21:25:05 +0100

    remove unused application helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,28 +15,6 @@ module ApplicationHelper
     [u.name, u.showcase_text, u.url, u.twitter.try(:profile_url)].map { |x| (x.nil? || x.strip.blank?) ? nil : x.strip }.compact.join(", ")
   end
 
-  def positivize_zero_val(val)
-    rule = { symbol_before_without_space: false, sign_before_symbol: true, sign_positive: true }
-
-    if val.cents != 0
-      return val.format(rule)
-    else
-      return "+#{val.format(rule)}"
-    end
-  end
-
-  def top_left_link_to(text, path)
-    link_to text, path, class: 'top-left-link'
-  end
-
-  def top_right_link_to(path)
-    link_to fa_icon('angle-double-right'), path, class: 'top-right-link'
-  end
-
-  def side_link_to(path)
-    link_to fa_icon('angle-double-right'), path, class: 'side-link'
-  end
-
   def simple_vertical_form_for(resource, options = {}, &block)
     options = options.merge(html: { class: 'form-vertical' }, wrapper: :vertical_form, wrapper_mappings: { boolean: :vertical_boolean })
     simple_form_for(resource, options, &block)
