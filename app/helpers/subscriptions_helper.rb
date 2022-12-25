561f2867c4fb3e5a319e7f713d3102da5092fd8e
Theodore Keloglou <zf@sirodoht.com>
2022-12-25 17:09:57 +0100

    change ruby hash syntax in subscription

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -2,11 +2,11 @@ module SubscriptionsHelper
   def stripe_button(name, description, email, amount, label)
     content_tag :script, '', src: 'https://checkout.stripe.com/checkout.js', class: 'stripe-button', data: {
       key: STRIPE_PUBLIC_KEY,
-      name: name,
+      name:,
       currency: 'gbp',
-      description: description,
-      email: email,
-      amount: amount,
+      description:,
+      email:,
+      amount:,
       'panel-label': label
     }
   end
