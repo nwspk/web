df973d8ba21c9ab58e76715d4f77446ff4c8ee6e
Theodore Keloglou <zf@sirodoht.com>
2023-01-12 21:20:22 +0000

    cleanup vertical form method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def plaintext_list_user_item(u)
-    [u.name, u.showcase_text, u.url, u.twitter.try(:profile_url)].map { |x| (x.nil? || x.strip.blank?) ? nil : x.strip }.compact.join(", ")
-  end
-
-  def simple_vertical_form_for(resource, options = {}, &block)
-    options = options.merge(html: { class: 'form-vertical' }, wrapper: :vertical_form, wrapper_mappings: { boolean: :vertical_boolean })
-    simple_form_for(resource, options, &block)
-  end
 end
--- config/initializers/gdf_renderer.rb
+++
@@ -1,5 +0,0 @@
-require 'action_controller/metal/renderers'
-
-ActionController.add_renderer :gdf do |gdf, options|
-  self.response_body = gdf.respond_to?(:to_gdf) ? gdf.to_gdf(options) : gdf
-end
