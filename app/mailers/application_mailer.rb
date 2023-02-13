2ef142a496298851d0dbe90629fa662dec9e6af0
Theodore Keloglou <zf@sirodoht.com>
2023-02-13 10:01:55 -0800

    update ed's email in more places

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'NWSPK <notifications@nwspk.com>', reply_to: 'ed@nwspk.com'
+  default from: 'NWSPK <notifications@nwspk.com>', reply_to: 'ed@newspeak.house'
   layout 'mailer'
 end
