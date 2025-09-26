49b05b366869707791c8a10397325ac4929dc359
edsaperia <edsaperia@gmail.com>
2025-09-26 11:35:54 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,16 +13,21 @@ Rails.application.routes.draw do
   get 'residency', to: 'home#residency', as: :residency
   get 'residents', to: 'home#residents', as: :residents
   get 'scholarships', to: 'home#scholarships', as: :scholarships
-  get '2023', to: 'home#course2023', as: :course2023
-  get '2024', to: 'home#course2024', as: :course2024
-  get '2025', to: 'home#course2025', as: :course2025
   get 'donate', to: redirect('https://donate.stripe.com/cN25lPeUe3qO5zyfYY'), as: :donate_redirect
   get 'shop', to: redirect('https://nwspk.myshopify.com/'), as: :shop
   get 'feedback', to: redirect('https://forms.gle/NLZ8JQdDFeuToWgF6'), as: :feedback
-  get 'fieldwork25', to: redirect('https://docs.google.com/document/d/19eLJIlsOuKmC0sMlSqC-C8gGmI9XIOtS_8jQ5pHBuWc/edit?usp=sharing'), as: :fieldwork25
   get 'rationclub', to: redirect('https://forms.gle/T3rXorsrb4gXKazv9'), as: :rationclub
   get 'library', to: 'home#library', as: :library
   get 'jobs', to: 'home#jobs', as: :jobs
+  
+  get '2023', to: 'home#course2023', as: :course2023
+  
+  get '2024', to: 'home#course2024', as: :course2024
+  
+  get '2025', to: 'home#course2025', as: :course2025
+  get 'fieldwork25', to: redirect('https://docs.google.com/document/d/19eLJIlsOuKmC0sMlSqC-C8gGmI9XIOtS_8jQ5pHBuWc'), as: :fieldwork25
+  get 'pairwork25', to: redirect('https://docs.google.com/document/d/1ZSf8g-G3rbklbpEDq_WFCykOUy921ZQwb9YZvFPbhbw'), as: :pairwork25 
+
   post 'webhooks', to: 'webhooks#index'
 
   # LCPT routes
